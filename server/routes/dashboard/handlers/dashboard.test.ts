dc8968c98e62b450e35d3bfc90ebab981ca78b72
AlexYoungmanMoJ <alexander.youngman@digital.justice.gov.uk>
2025-12-02 11:35:29 +0000

    Setup dashboard page

---
+++ server/routes/dashboard/handlers/dashboard.test.ts
@@ -0,0 +1,25 @@
+import { Request, Response } from 'express'
+import DashboardHandler from './dashboard'
+
+jest.mock('../../../services/auditService')
+
+describe('GET /dashboard', () => {
+  const handler = new DashboardHandler()
+  let req: Request
+  let res: Response
+
+  beforeEach(() => {
+    res = {
+      render: jest.fn(),
+    } as unknown as Response
+  })
+
+  afterEach(() => {
+    jest.resetAllMocks()
+  })
+
+  it('should render dashboard page', async () => {
+    await handler.get(req, res)
+    expect(res.render).toHaveBeenCalledWith('pages/dashboard/dashboard', expect.any(Object))
+  })
+})
---
+++ server/routes/dashboard/handlers/dashboard.ts
@@ -0,0 +1,21 @@
+import { Request, Response } from 'express'
+
+export default class DashboardHandler {
+  constructor() {}
+
+  public get = async (req: Request, res: Response) => {
+    const prisonPopulation = 1200
+    const payTypes = [
+      {
+        key: 'LTS',
+        name: 'Long term sick',
+        prisonerCount: 12,
+        url: '/add/long-term-sick',
+      },
+    ]
+    return res.render('pages/dashboard/dashboard', {
+      prisonPopulation,
+      payTypes,
+    })
+  }
+}
