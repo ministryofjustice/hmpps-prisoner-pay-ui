362832460b993738352ac042600d85288ef6fe70
AlexYoungmanMoJ <alexander.youngman@digital.justice.gov.uk>
2025-12-02 11:52:03 +0000

    Ignored unused services arg

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -3,6 +3,7 @@ import { Router } from 'express'
 import type { Services } from '../services'
 import DashboardRoutes from './dashboard'
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export default function routes(services: Services): Router {
   const router = Router()
 
