ccad4463c254f09039d546f67c9aa9a770dab56c
AlexYoungmanMoJ <alexander.youngman@digital.justice.gov.uk>
2025-12-12 11:23:55 +0000

    Update int test file

--- integration_tests/pages/register/addPrisonerPage.ts
+++ integration_tests/pages/register/addPrisonerPage.ts
@@ -4,8 +4,11 @@ import AbstractPage from '../abstractPage'
 export default class AddPrisonerPage extends AbstractPage {
   readonly header: Locator
 
+  readonly searchBox: Locator
+
   private constructor(page: Page) {
     super(page)
-    this.header = page.locator('h1', { hasText: 'Page header' })
+    this.header = page.locator('h1', { hasText: 'Who do you want to add?' })
+    this.searchBox = page.getByTestId('query')
   }
 }
