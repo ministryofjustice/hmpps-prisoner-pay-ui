5f40c7224bb8ea057be223bdb1f45746503622b6
AlexYoungmanMoJ <alexander.youngman@digital.justice.gov.uk>
2025-12-16 16:43:42 +0000

    Added missing tests

--- server/utils/utils.test.ts
+++ server/utils/utils.test.ts
@@ -1,4 +1,5 @@
-import { convertToTitleCase, initialiseName } from './utils'
+import { NameFormatStyle } from './helpers/nameFormatStyle'
+import { convertToTitleCase, formatFirstLastName, formatName, initialiseName } from './utils'
 
 describe('convert to title case', () => {
   it.each([
@@ -26,5 +27,143 @@ describe('initialise name', () => {
     ['Double barrelled', 'Robert-John Smith-Jones-Wilson', 'R. Smith-Jones-Wilson'],
   ])('%s initialiseName(%s, %s)', (_: string, a: string, expected: string) => {
     expect(initialiseName(a)).toEqual(expected)
+  })
+})
+
+describe('formatName', () => {
+  it.each([
+    ['All names (LastCommaFirst)', 'John', undefined, 'Smith', NameFormatStyle.lastCommaFirst, false, 'Smith, John'],
+    [
+      'Double barrelled last name (LastCommaFirst)',
+      'Jane',
+      undefined,
+      'Smith-Doe',
+      NameFormatStyle.lastCommaFirst,
+      true,
+      '<strong>Smith-Doe</strong>, Jane',
+    ],
+    [
+      'Multiple last names without hyphen (LastCommaFirst)',
+      'Jane',
+      undefined,
+      'Van Der Ploeg',
+      NameFormatStyle.lastCommaFirst,
+      false,
+      'Van Der Ploeg, Jane',
+    ],
+    [
+      'Multiple first names without hyphen (LastCommaFirst)',
+      'Jane Sarah',
+      undefined,
+      'Smith',
+      NameFormatStyle.lastCommaFirst,
+      false,
+      'Smith, Jane Sarah',
+    ],
+    [
+      'Multiple first names with hyphen (LastCommaFirst)',
+      'Sarah-Jane',
+      undefined,
+      'Smith',
+      NameFormatStyle.lastCommaFirst,
+      false,
+      'Smith, Sarah-Jane',
+    ],
+    ['Basic name (firstLast)', 'Sarah', undefined, 'Smith', NameFormatStyle.firstLast, false, 'Sarah Smith'],
+    [
+      'Double-barelled last name (firstLast)',
+      'Sarah',
+      undefined,
+      'Smith-Jones',
+      NameFormatStyle.firstLast,
+      false,
+      'Sarah Smith-Jones',
+    ],
+    [
+      'Two last names (firstLast)',
+      'Sarah',
+      undefined,
+      'Smith Jones',
+      NameFormatStyle.firstLast,
+      false,
+      'Sarah Smith Jones',
+    ],
+    [
+      'Two first names (firstLast)',
+      'Sarah Jane',
+      undefined,
+      'Smith',
+      NameFormatStyle.firstLast,
+      false,
+      'Sarah Jane Smith',
+    ],
+    [
+      'Two first names with hyphen (firstLast)',
+      'Sarah-Jane',
+      undefined,
+      'Smith',
+      NameFormatStyle.firstLast,
+      false,
+      'Sarah-Jane Smith',
+    ],
+    [
+      'First and last names (LastCommaFirstMiddle)',
+      'John',
+      undefined,
+      'Smith',
+      NameFormatStyle.lastCommaFirstMiddle,
+      false,
+      'Smith, John',
+    ],
+    [
+      'First and last names (lastCommaFirst)',
+      'John',
+      undefined,
+      'Smith',
+      false,
+      NameFormatStyle.lastCommaFirst,
+      'John Smith',
+    ],
+    ['First and last names (no style)', 'John', undefined, 'Smith', false, undefined, 'John Smith'],
+    [
+      'All names (LastCommaFirstMiddle)',
+      'John',
+      'James',
+      'Smith',
+      NameFormatStyle.lastCommaFirstMiddle,
+      false,
+      'Smith, John James',
+    ],
+    ['Apostrophe (no style)', 'JOHN', 'JAMES', "O'sullivan", undefined, false, "John James O'Sullivan"],
+  ])(
+    '%s: formatName(%s, %s, %s, %s, %s, %s, %s)',
+    (
+      _: string,
+      firstName: string,
+      middleNames: string,
+      lastName: string,
+      nameFormatStyle: NameFormatStyle,
+      boldLastName: boolean,
+      expected: string,
+    ) => {
+      expect(formatName(firstName, middleNames, lastName, nameFormatStyle, boldLastName)).toEqual(expected)
+    },
+  )
+})
+
+describe('format first & lastName', () => {
+  it.each([
+    ['Lower case', 'robert', 'smith', 'Robert Smith'],
+    ['Upper case', 'ROBERT', 'SMITH', 'Robert Smith'],
+    ['Mixed case', 'RoBErT', 'sMItH', 'Robert Smith'],
+    ['Multiple words', 'RobeRT JOhN', 'sMItH', 'Robert John Smith'],
+    ['Leading spaces', '  RobeRT', '  smiTH', 'Robert Smith'],
+    ['Trailing spaces', 'RobeRT  ', 'SmItH ', 'Robert Smith'],
+    ['Single value', 'robert', undefined, 'Robert'],
+    ['Single value full name', 'robert smith', undefined, 'Robert Smith'],
+    ['Hyphenated', 'Robert-John', 'SmiTH-jONes-WILSON', 'Robert-John Smith-Jones-Wilson'],
+    ['Sentence', 'John smith is here', undefined, 'John Smith Is Here'],
+  ])('%s formatFirstLastName(%s, %s)', (_: string, a: string, b: string, expected: string) => {
+    expect(formatFirstLastName(a, b)).toEqual(expected)
   })
 })
