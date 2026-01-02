44baad9e969e737040b378e53910dde695e62049
AlexYoungmanMoJ <alexander.youngman@digital.justice.gov.uk>
2026-01-02 13:17:15 +0000

    Added add prisoner playwright journey

--- integration_tests/pages/register/addPrisonerPage.ts
+++ integration_tests/pages/register/addPrisonerPage.ts
@@ -1,4 +1,4 @@
-import { type Locator, type Page } from '@playwright/test'
+import { expect, type Locator, type Page } from '@playwright/test'
 import AbstractPage from '../abstractPage'
 
 export default class AddPrisonerPage extends AbstractPage {
@@ -6,9 +6,20 @@ export default class AddPrisonerPage extends AbstractPage {
 
   readonly searchBox: Locator
 
+  readonly searchButton: Locator
+
   private constructor(page: Page) {
     super(page)
     this.header = page.locator('h1', { hasText: 'Who do you want to add?' })
     this.searchBox = page.getByTestId('query')
+    this.searchButton = page.getByRole('button', { name: 'Search' })
+  }
+
+  static async verifyOnPage(page: Page): Promise<AddPrisonerPage> {
+    const addPrisonerPage = new AddPrisonerPage(page)
+    await expect(addPrisonerPage.header).toBeVisible()
+    await expect(addPrisonerPage.searchBox).toBeVisible()
+    await expect(addPrisonerPage.searchButton).toBeVisible()
+    return addPrisonerPage
   }
 }
--- integration_tests/pages/register/confirmedAddPrisonerPage.ts
+++ integration_tests/pages/register/confirmedAddPrisonerPage.ts
@@ -1,4 +1,4 @@
-import { type Locator, type Page } from '@playwright/test'
+import { expect, type Locator, type Page } from '@playwright/test'
 import AbstractPage from '../abstractPage'
 
 export default class ConfirmedAddPrisonerPage extends AbstractPage {
@@ -7,5 +7,11 @@ export default class ConfirmedAddPrisonerPage extends AbstractPage {
   private constructor(page: Page) {
     super(page)
     this.header = page.locator('h1', { hasText: 'will now be paid' })
+  }
+
+  static async verifyOnPage(page: Page): Promise<ConfirmedAddPrisonerPage> {
+    const confirmedAddPrisonerPage = new ConfirmedAddPrisonerPage(page)
+    await expect(confirmedAddPrisonerPage.header).toBeVisible()
+    return confirmedAddPrisonerPage
   }
 }
