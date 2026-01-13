10b49bcedae812075157db2c8fa64c5de931c262
AlexYoungmanMoJ <alexander.youngman@digital.justice.gov.uk>
2026-01-13 09:52:45 +0000

    Added actual pay period count to dashboard

--- server/routes/dashboard/handlers/dashboard.ts
+++ server/routes/dashboard/handlers/dashboard.ts
@@ -1,13 +1,24 @@
 import { Request, Response } from 'express'
+import { format } from 'date-fns'
 import OrchestratorService from '../../../services/orchestratorService'
-import { getAllPayTypes } from '../../../utils/payTypeUtils'
+import { getAllPayTypes, getPayTypeBySlug } from '../../../utils/payTypeUtils'
 
 export default class DashboardHandler {
   constructor(private readonly orchestratorService: OrchestratorService) {}
 
   GET = async (req: Request, res: Response) => {
-    const payTypes = getAllPayTypes()
-    const prisonPopulation = 1200
+    const { activeCaseLoadId } = res.locals.user
+    const prisonPopulation = 1000
+    const paySummary = await this.orchestratorService.getPayStatusPeriodsByType(
+      format(new Date(), 'yyyy-MM-dd'),
+      activeCaseLoadId,
+    )
+    const payTypes = getAllPayTypes().map(payType => {
+      return {
+        ...payType,
+        prisonerCount: paySummary.filter(period => period.type === payType.type).length,
+      }
+    })
 
     return res.render('pages/dashboard/dashboard', {
       prisonPopulation,
