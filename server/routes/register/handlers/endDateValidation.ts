9ea6164bc5f52fd52ed4f646c545c588221b1704
AlexYoungmanMoJ <alexander.youngman@digital.justice.gov.uk>
2026-01-14 15:21:51 +0000

    SAA-3971: Added validation to end date selection

--- server/routes/register/handlers/endDateValidation.ts
+++ server/routes/register/handlers/endDateValidation.ts
@@ -1,3 +1,4 @@
+import { isBefore, isDate, isValid, parse } from 'date-fns'
 import { FormError } from '../../../@types/template'
 
 const errors: { [key: string]: FormError } = {
@@ -5,10 +6,30 @@ const errors: { [key: string]: FormError } = {
     href: '#endDateSelection',
     text: 'Select end date option',
   },
+  ENTER_DATE: {
+    href: '#selectedDate',
+    text: 'Enter or select a date',
+  },
+  VALID_DATE: {
+    href: '#selectedDate',
+    text: 'Enter a real date',
+  },
+  FUTURE_DATE: {
+    href: '#selectedDate',
+    text: 'The date must be after today',
+  },
 }
 
-export default function validateForm(endDateSelection: string): FormError | null {
+export default function validateForm(endDateSelection: string, selectedDate: string): FormError | null {
   if (!endDateSelection) return errors.SELECT_OPTION
+
+  if (endDateSelection === 'yes' && !selectedDate) return errors.ENTER_DATE
+
+  const parsedDate = parse(selectedDate, 'dd/MM/yyyy', new Date())
+
+  if (endDateSelection === 'yes' && !isValid(parsedDate)) return errors.VALID_DATE
+
+  if (endDateSelection === 'yes' && isValid(parsedDate) && isBefore(parsedDate, new Date())) return errors.FUTURE_DATE
 
   return null
 }
