9977c4a9eb91bbb578b362a1eae37950b8e474ff
AlexYoungmanMoJ <alexander.youngman@justice.gov.uk>
2026-01-16 11:31:32 +0000

    Updated minor package versions (#38)
    * Updated client-sqs, hmpps-auth-clients, body-parser
    
    * Update express
    
    * Update govuk-frontend
    
    * Update http-errors redis superagent
    
    * Updated eslint-config-hmpps playwright/test types/node
    
    * Update esbuild
    
    * Revert esbuild, Update esbuild-sass-plugin
    
    * Revert esbuild-sass-plugin
    
    * Added diff override
    
    * Update allowed scripts
    
    * Minor version updates

--- .allowed-scripts.mjs
+++ .allowed-scripts.mjs
@@ -2,9 +2,9 @@ import { configureAllowedScripts } from '@ministryofjustice/hmpps-npm-script-all
 
 export default configureAllowedScripts({
   allowlist: {
-    'node_modules/@parcel/watcher@2.5.1': 'ALLOW',
+    'node_modules/@parcel/watcher@2.5.4': 'ALLOW',
     'node_modules/dtrace-provider@0.8.8': 'ALLOW',
-    'node_modules/esbuild@0.27.0': 'ALLOW',
+    'node_modules/esbuild@0.27.2': 'ALLOW',
     'node_modules/fsevents@2.3.3': 'ALLOW',
     'node_modules/playwright/node_modules/fsevents@2.3.2': 'ALLOW',
     'node_modules/unrs-resolver@1.11.1': 'ALLOW',
