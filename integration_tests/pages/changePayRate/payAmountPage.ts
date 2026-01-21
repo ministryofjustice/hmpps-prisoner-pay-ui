e53a351ed847e5ac1e0f942c6dd9b9c9a3213068
AlexYoungmanMoJ <alexander.youngman@digital.justice.gov.uk>
2026-01-21 09:04:05 +0000

    Updated playwright page

--- integration_tests/pages/changePayRate/payAmountPage.ts
+++ integration_tests/pages/changePayRate/payAmountPage.ts
@@ -2,10 +2,28 @@ import { type Locator, type Page } from '@playwright/test'
 import AbstractPage from '../abstractPage'
 
 export default class PayAmountPage extends AbstractPage {
-  readonly header: Locator
+  readonly payAmountInput: Locator
+
+  readonly continueButton: Locator
+
+  readonly cancelLink: Locator
 
   private constructor(page: Page) {
     super(page)
-    this.header = page.locator('h1', { hasText: 'Page header' })
+    this.payAmountInput = page.locator('#payAmount')
+    this.continueButton = page.locator('button', { hasText: 'Continue' })
+    this.cancelLink = page.locator('a', { hasText: 'Cancel' })
+  }
+
+  async enterPayAmount(amount: string): Promise<void> {
+    await this.payAmountInput.fill(amount)
+  }
+
+  async clickContinue(): Promise<void> {
+    await this.continueButton.click()
+  }
+
+  async clickCancel(): Promise<void> {
+    await this.cancelLink.click()
   }
 }
