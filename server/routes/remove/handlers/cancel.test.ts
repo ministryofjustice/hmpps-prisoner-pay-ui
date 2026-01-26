5de2de8fcfff047006dfc78c810e29be09619ac8
Ashley Gyngell <ashley.gyngell@justice.gov.uk>
2026-01-26 14:47:10 +0000

    SAA-3968: Implement cancel removal of pay type page

---
+++ server/routes/remove/handlers/cancel.test.ts
@@ -0,0 +1,68 @@
+import { Request, Response } from 'express'
+import CancelHandler from './cancel'
+
+describe('CancelRemovalHandler', () => {
+  let handler: CancelHandler
+  let req: Partial<Request>
+  let res: Partial<Response>
+
+  beforeEach(() => {
+    handler = new CancelHandler()
+    req = {
+      params: { payTypeSlug: 'long-term-sick' },
+      query: {
+        query: 'test',
+      },
+      body: {},
+      session: {
+        selectedPrisoner: {
+          firstName: 'Joe',
+          lastName: 'Bloggs',
+        },
+      },
+    } as unknown as Partial<Request>
+
+    res = {
+      render: jest.fn(),
+      redirect: jest.fn(),
+    }
+  })
+
+  describe('GET', () => {
+    it('should render the correct view', async () => {
+      await handler.GET(req as Request, res as Response)
+
+      expect(res.render).toHaveBeenCalledWith('pages/remove/cancel', {
+        prisonerName: 'Joe Bloggs',
+      })
+    })
+  })
+
+  describe('POST', () => {
+    it('should redirect to pay overview page when selecting that you want to cancel the application', async () => {
+      req.body.choice = 'yes'
+
+      await handler.POST(req as Request, res as Response)
+
+      expect(res.redirect).toHaveBeenCalledWith('/long-term-sick/pay-overview')
+    })
+
+    it('should redirect to check page after selecting that you do not want to cancel the application', async () => {
+      req.body.choice = 'no'
+      req.session.returnTo = 'check'
+
+      await handler.POST(req as Request, res as Response)
+
+      expect(res.redirect).toHaveBeenCalledWith('check')
+    })
+
+    it('should redirect to dashboard page after selecting No and returnTo is null ', async () => {
+      req.body.choice = 'no'
+      req.session.returnTo = undefined
+
+      await handler.POST(req as Request, res as Response)
+
+      expect(res.redirect).toHaveBeenCalledWith('/')
+    })
+  })
+})
